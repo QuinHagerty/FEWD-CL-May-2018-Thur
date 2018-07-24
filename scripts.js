e9f1b1771d1a779e59366a5cfb68ded867bdc4fa
Quin Hagerty <hagertyq@gmail.com>
2018-07-24 18:59:28 -0400

    added more comments to my HTLM and JS. I also fixed a few problems with my header and functions connected to it.

--- scripts.js
+++ scripts.js
@@ -1,23 +1,25 @@
-  $(document).ready(function(){
-    $('.flipBurger').click(function(){
-      $('.flipBurger').toggleClass('active')
-    })
+$(document).ready(function(){
+
+  const mql = window.matchMedia("(min-width:769px)");
+
+  //START site name return to top
+  $('.return-to-top').click(function(){
+    $('html,body').scrollTop(0)
   })
-  //burger menu javascript
-  $(document).ready(function(){
-    $('.return-to-top').click(function(){
-      $('html,body').scrollTop(0)
-    })
+  //END site name return to top
+
+  //START burger menu JS
+  $('.flipBurger').click(function(){
+    $('.flipBurger').toggleClass('active')
   })
-  //site name return to top
-  $(document).ready(function(){
-    $('.flipBurger-container, .main-nav li').click(function(){
-      $('.main-nav').toggleClass('dropped')
-    })
+  //END burger menu JS
+
+  //START burger menu drop down
+  $('.flipBurger-container, .main-nav li').click(function(){
+    if (!mql.matches) {
+    $('.main-nav').toggleClass('dropped')
+    }
   })
-/*  $(document).ready(function(){
-    $('.return-to-top').click(function(){
-      $('html,body').scrollTop(0)
-    })
+  //END burger menu drop down
+
   })
-*/
